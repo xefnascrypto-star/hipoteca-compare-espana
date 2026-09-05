8d84e337e8402d06e15a58c56be75ce37117e5f2
xefnascrypto-star <xefnascrypto@gmail.com>
2026-09-06 01:52:20 +0200

    Añade Consent Mode v2 por defecto para Google

---
+++ consent-mode.js
@@ -0,0 +1,29 @@
+window.dataLayer=window.dataLayer||[];
+function gtag(){dataLayer.push(arguments);}
+gtag('consent','default',{
+  ad_storage:'denied',
+  analytics_storage:'denied',
+  ad_user_data:'denied',
+  ad_personalization:'denied',
+  wait_for_update:500
+});
+gtag('set','ads_data_redaction',true);
+gtag('set','url_passthrough',true);
+window.HipotecaCompareConsent={
+  grant:function(){
+    gtag('consent','update',{
+      ad_storage:'granted',
+      analytics_storage:'granted',
+      ad_user_data:'granted',
+      ad_personalization:'granted'
+    });
+  },
+  deny:function(){
+    gtag('consent','update',{
+      ad_storage:'denied',
+      analytics_storage:'denied',
+      ad_user_data:'denied',
+      ad_personalization:'denied'
+    });
+  }
+};
